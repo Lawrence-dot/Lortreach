9fd89cabdc1781b5063fb93f9b160f3232008e14
Ojediran Lawrence <damilareojediran3@gmail.com>
2022-09-14 13:08:45 +0100

    ll

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
       "hidden", "sm:hidden", "flex", "block", "absolute", "sm:block", "ml-auto", "pb-3", "ml-2", "my-auto",
       "m-2", "bg-[#1A1036]", "mb-auto", "mx-5", "sm:mx-10", "lg:mx-16", "hover:bg-primary", "hover:text-white",
       "inline", "mx-1", "text-black", "m-3", "text-[#FF002E]", "rounded", "rounded-lg", "lg:mr-auto", "pt-2",
-      "sm:pt-3", "border-l-2", "border-primary", "w-32", "text-sm", "text-md", "text-xl", "text-2xl", "text-3xl"
+      "sm:pt-3", "border-l-2", "border-primary", "w-32", "text-sm", "text-md", "text-xl", "text-2xl", "text-3xl",
+      "ml-1"
     ],
   theme: {
     extend: {
