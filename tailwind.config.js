5d421aab50e935f8463e0f4f314423028b564fb0
Ojediran Lawrence <damilareojediran3@gmail.com>
2022-09-14 12:45:06 +0100

    ll

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
   safelist:
     ["hidden", "sm:hidden", "flex", "block", "absolute", "sm:block", "ml-auto", "pb-3", "ml-2", "my-auto", "m-2",
       "bg-[#1A1036]", "mb-auto", "mx-5", "sm:mx-10", "lg:mx-16", "hover:bg-primary", "hover:text-white",
-      "inline", "mx-1", "text-black", "m-3", "text-[#FF002E]", "rounded", "lg:mr-auto", "pt-2", "sm:pt-3", "border-l-2", "border-primary"
+      "inline", "mx-1", "text-black", "m-3", "text-[#FF002E]", "rounded", "lg:mr-auto", "pt-2", "sm:pt-3", "border-l-2",
+      "border-primary", "w-32"
     ],
   theme: {
     extend: {
